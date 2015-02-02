ca3ce40d35fb3cd9334f574172238cfb9bf9a56d
kofwhgh <kofwhgh@1b7c1bb3-bd57-12b3-5fb4-2f6cebb7c126>
2015-02-02 10:19:05 +0000

    기존 id개념을 playid로 변경 이유 : db id를 살리기 위해서

--- mps/src/main/webapp/resources/mps/js/common/asyncHttpModule.js
+++ mps/src/main/webapp/resources/mps/js/common/asyncHttpModule.js
@@ -8,7 +8,10 @@ asyncHttpModule.factory('asyncHttpService', function($q, $http, $rootScope) {
 	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 	
 	var asyncHttpService = {};
-			
+	
+	/**
+	 * $http.post를 통해 호출하며 $.param으로 param을 감싸서 호출 할때 사용한다. Controller : @requestParam, @modelAttribute에서 사용
+	 */
 	asyncHttpService.httpPost = function(url, params, callback) {
 		params = params||{};
 
@@ -22,22 +25,9 @@ asyncHttpModule.factory('asyncHttpService', function($q, $http, $rootScope) {
 	    });
 	};
 	
-	asyncHttpService.httpGet = function(url, params, callback) {
-		params = params||{};
-
-		$http.get(url, params, {
-			headers: {
-				   'Content-Type': "application/json"
-				}
-	    }).success(function(data, status) {
-	    	if(callback) {
-	    		callback(data);
-	    	}
-	    }).error(function(data, status) {
-	    	alert("[Error] " + data);
-	    });
-	};
-	
+	/**
+	 * $http.post를 통해 호출하며 Content-Type을 application/json을 통해서 올릴때 사용한다. Controller는 @responseBody를 사용해서 통으로 받아올때 사용한다.
+	 */
 	asyncHttpService.httpPostJson = function(url, params, callback) {
 		params = params||{};
 
@@ -67,6 +57,20 @@ asyncHttpModule.factory('asyncHttpService', function($q, $http, $rootScope) {
 	    });
 	    
 	    return defer.promise;
+	};
+	
+	asyncHttpService.httpGet = function(url, params, callback) {
+		params = params||{};
+		
+		$http.get(url, params)
+	    .success( function (data) {
+	    	if(callback) {
+	    		callback(data);
+	    	}
+	    })
+	    .error( function (data, status) {
+	    	alert("[Error] " + data);
+	    });
 	};
 			
 	return asyncHttpService;
