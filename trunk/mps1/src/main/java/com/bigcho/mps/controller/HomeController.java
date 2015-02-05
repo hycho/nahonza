b286e8aefe68c985cbe32002a00511c4ec3f6bbb
kofwhgh <kofwhgh@1b7c1bb3-bd57-12b3-5fb4-2f6cebb7c126>
2015-02-05 11:57:48 +0000

    시큐리티 적용중..

--- trunk/mps1/src/main/java/com/bigcho/mps/controller/HomeController.java
+++ trunk/mps1/src/main/java/com/bigcho/mps/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.bigcho.mps.controller;
 
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		return "pages/login";
 	}
 	
 }
